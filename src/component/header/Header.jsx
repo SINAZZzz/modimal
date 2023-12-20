17dca981594867320904d003771dc4d287653063
Sina Zinsaz <sinazinsaz1@gamil.com>
2023-12-20 15:12:55 +0330

    add head

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,113 +1,101 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
+//  MUI
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+// icons
+import SearchIcon from "@mui/icons-material/Search";
+import Person4OutlinedIcon from "@mui/icons-material/Person4Outlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 
 // logo
 import { logo } from "../../assets/images";
 
-const drawerWidth = 240;
-const navItems = ["Home", "About", "Contact"];
+const navIcons = [
+  {
+    icon: <SearchIcon />,
+  },
+  {
+    icon: <Person4OutlinedIcon />,
+  },
+  {
+    icon: <FavoriteBorderOutlinedIcon />,
+  },
+  {
+    icon: <ShoppingBagOutlinedIcon />,
+  },
+];
+const navItems = [
+  "Collection",
+  "New In",
+  "Modiweek",
+  "Plus Size",
+  "Sustainability",
+];
 
-function Header(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}></Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
+function Header() {
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
+    <Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="2rem"
+        sx={{ background: "#5A6D57" }}
+      >
+        <Typography variant="body1" color="white" sx={{ fontSize: 14 }}>
+          Enjoy Free Shipping On All Orders
+        </Typography>
+      </Box>
+      <Box
+        color="inherit"
+        sx={{
+          height: "40%",
+          background: "white",
+          justifyContent: "center",
+          my: "0.5rem",
+        }}
+      >
+        <Toolbar
+          display="flex"
+          sx={{ justifyContent: "space-between", mx: "5%" }}
+        >
+          <Box>
             <img src={logo} alt="" />
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          </Box>
+          <Box display="flex">
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
+              <Typography
+                key={item}
+                sx={{
+                  color: "#000",
+                  paddingLeft: "5rem",
+                  fontWeight: 400,
+                  fontSize: 18,
+                }}
+              >
                 {item}
-              </Button>
+              </Typography>
+            ))}
+          </Box>
+          <Box display="flex">
+            {navIcons.map((item) => (
+              <Typography
+                key={item}
+                sx={{
+                  color: "#000",
+                  paddingLeft: "2rem",
+                }}
+              >
+                {item.icon}
+              </Typography>
             ))}
           </Box>
         </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
+      </Box>
     </Box>
   );
 }
-
-Header.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
 
 export default Header;
