6739e3be0a4c85a14e4b28bb9e3453bc9e2dfc74
AmirHossein137 <122604637+AmirHossein137@users.noreply.github.com>
2023-12-24 12:33:45 +0330

    1402/10/03

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,10 +1,10 @@
 //  MUI
 import * as React from "react";
-import Toolbar from "@mui/material/Toolbar";
+// import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
 
 // icons
 import SearchIcon from "@mui/icons-material/Search";
@@ -45,68 +45,58 @@ function Header() {
         justifyContent="center"
         alignItems="center"
         height="2rem"
+        mb={2}
         sx={{ background: "#5A6D57" }}
       >
         <Typography variant="body1" color="white" sx={{ fontSize: 14 }}>
           Enjoy Free Shipping On All Orders
         </Typography>
       </Box>
-      <Box>
-          <Box sx={{ width : '100%' }}>
-            <Grid container>
-              <Grid item xs={2} p={1} bgcolor='red'>A</Grid>
-              <Grid item xs={7} p={1} bgcolor='blue'>B</Grid>
-              <Grid item xs={3} p={1} bgcolor='green'>C</Grid>
-            </Grid>
-          </Box>
+      <Box sx={{ flexGrow: 1 }} display="flex" justifyContent="center">
+        <Grid container maxWidth="1350px" spacing={2} alignItems="center">
+          <Grid item xs={3}>
+            <Box p={1}>
+              <img src={logo} alt="" sx={{ width: "100%" }} />
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              p={1}
+            >
+              {navItems.map((item, index) => (
+                <Typography
+                  key={index}
+                  sx={{
+                    color: "#000",
+                    fontWeight: 400,
+                    fontSize: 16,
+                  }}
+                >
+                  {item}
+                </Typography>
+              ))}
+            </Box>
+          </Grid>
+          <Grid item xs={3}>
+            <Box display="flex" justifyContent='flex-end' p={1}>
+              {navIcons.map((item) => (
+                <Typography
+                  key={item}
+                  sx={{
+                    color: "#000",
+                    paddingLeft: "2rem",
+                  }}
+                >
+                  {item.icon}
+                </Typography>
+              ))}
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
-      {/* <Box
-        color="inherit"
-        sx={{
-          height: "40%",
-          background: "white",
-          justifyContent: "center",
-          my: "0.5rem",
-        }}
-      >
-
-        <Toolbar
-          display="flex"
-          sx={{ justifyContent: "space-between", mx: "5%" }}
-        >
-          <Box>
-            <img src={logo} alt="" />
-          </Box>
-          <Box display="flex">
-            {navItems.map((item , index) => (
-              <Typography
-                key={index}
-                sx={{
-                  color: "#000",
-                  paddingLeft: "5rem",
-                  fontWeight: 400,
-                  fontSize: 18,
-                }}
-              >
-                {item}
-              </Typography>
-            ))}
-          </Box>
-          <Box display="flex">
-            {navIcons.map((item) => (
-              <Typography
-                key={item}
-                sx={{
-                  color: "#000",
-                  paddingLeft: "2rem",
-                }}
-              >
-                {item.icon}
-              </Typography>
-            ))}
-          </Box>
-        </Toolbar>
-      </Box> */}
     </Box>
   );
 }
